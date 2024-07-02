a2c0167637e057c10b08ee4d9870d327a4772ceb
Lev Aleshkin <lev.aleshkin01@yandex.ru>
2024-07-02 17:50:39 +0300

    Commit_02072024_1750

--- MyLearningApp/Learning_1.cs
+++ MyLearningApp/Learning_1.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+string[] array = new string[5];
+string[] array2 = array;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = "array" + i;
+    Console.WriteLine(array2[i]);    
+}
+
