e231a10300a00b180ba211b0dbce39991077dbcf
Lev Aleshkin <lev.aleshkin01@yandex.ru>
2024-07-16 17:57:20 +0300

    Commit_16072024_1757

--- MyLearningApp/Learning_1.cs
+++ MyLearningApp/Learning_1.cs
@@ -222,6 +222,99 @@ static void WriteColoredMessage(string message, ConsoleColor color)
 // player.PickUp(gun);
 
 
+int balance = 0;
+int[] coinsQuantity = { 0, 0, 0, 0}; //1, 2, 5, 10
+
+
+
+int[] coinsValues = { 1, 2, 5, 10};
+PaymentType payment = PaymentType.Coins;
+string[] names = { "Шоколадка", "Газировка"
+};
+int[] prices = { 70, 60 };
+int[] availableQuantity = { 5, 2 };
+
+string command = "";
+while (true)
+{
+    //Console.Clear();
+    // try
+    // {
+    //     Console.Clear();
+    // }
+    // catch (IOException)
+    // {
+    //     Console.WriteLine("Не удалось очистить консоль.");
+    // }
+
+    Console.WriteLine($"Баланс {balance}");
+    Console.WriteLine("Введите команду:");
+    command = Console.ReadLine();
+if(command == "AddMoney")
+{
+}
+else if(command == "GetChange")
+{
+}
+else if (command.StartsWith("BuyGood"))
+{
+}
+else
+{
+    Console.WriteLine("Команда неопределена");
+}
+
+Console.ReadLine();
+}
+
+if(command == "AddMoney")
+{
+    switch (payment)
+    {
+        case PaymentType.Coins:
+        for(int i = 0; i < coinsValues.Length; i++)
+        {
+        Console.WriteLine($"Сколько монет номиналом {coinsValues[i]} вы хотите внести?");
+        int count = 0;
+
+        while
+        (!int.TryParse(Console.ReadLine(), out count))
+        {
+            Console.WriteLine("Вы ввели не число!");
+        }
+        coinsQuantity[i] += count;
+        balance += count *
+        coinsValues[i];
+        }
+        break;
+
+        case PaymentType.Card:
+        Console.WriteLine("Сколько снять свашей карты?");
+        int balanceDelta = 0;
+        while
+        (!int.TryParse(Console.ReadLine(),out balanceDelta))
+        {
+            Console.WriteLine("Вы ввелине число!");
+        }
+        balance += balanceDelta;
+        Console.WriteLine("Баланс успешно пополнен");
+        break;
+
+        default:
+        break;  
+    }
+}
+
+
+enum PaymentType
+{
+    Coins,
+    Card
+}
+
+
+
+
 
 
 
