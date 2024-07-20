8518986a0bb3094e1f8149abcc6cc9ad848380e7
Lex <lev.aleshkin01@yandex.ru>
2024-07-20 18:29:02 +0300

    Commit_20072024_1828

--- MyLearningApp/Learning_1.cs
+++ MyLearningApp/Learning_1.cs
@@ -222,124 +222,224 @@ static void WriteColoredMessage(string message, ConsoleColor color)
 // player.PickUp(gun);
 
 
-int         balance           = 0;
-PaymentType payment           = PaymentType.Coins;
-Boolean     bol               = true;
-string      command           = "";
-int[]       coinsQuantity     = { 0, 0, 0, 0}, //1, 2, 5, 10
-            coinsValues       = { 1, 2, 5, 10},
-            prices            = { 70, 60 },
-            availableQuantity = { 5, 2 };
-string[]    names             = { "Шоколадка", "Газировка"};
+// int         balance           = 0;
+// PaymentType payment           = PaymentType.Coins;
+// Boolean     bol               = true;
+// string      command           = "";
+// int[]       coinsQuantity     = { 0, 0, 0, 0}, //1, 2, 5, 10
+//             coinsValues       = { 1, 2, 5, 10},
+//             prices            = { 70, 60 },
+//             availableQuantity = { 5, 2 };
+// string[]    names             = { "Шоколадка", "Газировка"};
 
 
-while (bol)
+// while (bol)
+// {
+//     //Console.Clear();
+//     // try
+//     // {
+//     //     Console.Clear();
+//     // }
+//     // catch (IOException)
+//     // {
+//     //     Console.WriteLine("Не удалось очистить консоль.");
+//     // }
+
+//     Console.WriteLine($"Баланс {balance}");
+//     Console.WriteLine("Введите команду:");
+//     command = Console.ReadLine();
+//     if(command == "AddMoney")
+//     {
+//         bol = false;
+//     }
+//     else if(command == "GetChange")
+//     {
+//         bol = false;
+//     }
+//     else if (command.StartsWith("BuyGood"))
+//     {
+//         //Разбиение строки на единицы данных
+
+//         string[] rawData = command.Substring("BuyGood ".Length).Split(' ');
+
+//         //Сопоставление этих данных с переменными (и их типами)
+//         if(rawData.Length != 2)
+//         {
+//             Console.WriteLine("Неправильные аргументы команды");
+//             break;
+//         }
+
+//         int id = Convert.ToInt32(rawData[0]);
+//         int count = Convert.ToInt32(rawData[1]);
+
+//         //Проверка корректности этих данных
+//         //на основе текущего состояния модели.
+//         if(id < 0 || id >= names.Length)
+//         {
+//             Console.WriteLine("Такого товара нет");
+//             break;
+//         }
+
+//         if(count < 0 || count > availableQuantity[id])
+//         {
+//             Console.WriteLine("Нет такого количества");
+//             break;
+//         }
+
+//         //Выполнение
+//         if(balance >= prices[id] * count)
+//         {
+//             balance -= prices[id] * count;
+//             availableQuantity[id] -= count; 
+//         }
+
+//         bol = false;
+//     }
+//     else
+//     {
+//         Console.WriteLine("Команда неопределена");
+//     }
+
+//     //Console.ReadLine();
+// }
+
+// if(command == "AddMoney")
+// {
+//     switch (payment)
+//     {
+//         case PaymentType.Coins:
+//         for(int i = 0; i < coinsValues.Length; i++)
+//         {
+//             int count = 0;
+//             Console.WriteLine($"Сколько монет номиналом {coinsValues[i]} вы хотите внести?");            
+
+//             while(!int.TryParse(Console.ReadLine(), out count))
+//             {
+//                 Console.WriteLine("Вы ввели не число!");
+//             }
+            
+//             coinsQuantity[i] += count;
+//             balance += count * coinsValues[i];
+//         }
+//         break;
+
+//         case PaymentType.Card:
+//         int balanceDelta = 0;
+//         Console.WriteLine("Сколько снять свашей карты?");        
+        
+//         while(!int.TryParse(Console.ReadLine(),out balanceDelta))
+//         {
+//             Console.WriteLine("Вы ввелине число!");
+//         }
+
+//         balance += balanceDelta;
+//         Console.WriteLine("Баланс успешно пополнен");
+//         break;
+
+//         default:
+//         break;  
+//     }
+// }
+
+
+// enum PaymentType
+// {
+//     Coins,
+//     Card
+// }
+
+
+int balance = 0;
+int[] coinsQuantity = { 0, 0, 0, 0}; //1, 2, 5, 10
+int[] coinsValues = { 1, 2, 5, 10};
+string[] names = { "Шоколадка", "Газировка" };
+int[] prices = { 70, 60 };
+int[] availableQuantity = { 5, 2};
+
+PaymentType payment = PaymentType.Card;
+string command = "";
+
+while (true)
 {
-    //Console.Clear();
-    // try
-    // {
-    //     Console.Clear();
-    // }
-    // catch (IOException)
-    // {
-    //     Console.WriteLine("Не удалось очистить консоль.");
-    // }
-
+    Console.Clear();
     Console.WriteLine($"Баланс {balance}");
     Console.WriteLine("Введите команду:");
     command = Console.ReadLine();
     if(command == "AddMoney")
     {
-        bol = false;
+        switch (payment)
+        {
+            case PaymentType.Coins:
+                for(int i = 0; i < coinsValues.Length; i++)
+                {
+                    Console.WriteLine($"Сколько монет номиналом {coinsValues[i]} вы хотите внести?");
+                    int count = 0;
+                    while(!int.TryParse(Console.ReadLine(), out count))
+                    {
+                        Console.WriteLine("Вы ввели не число!");
+                    }
+                    coinsQuantity[i] += count;
+                    balance +=  count * coinsValues[i];
+                }
+            break;
+
+            case PaymentType.Card:
+                Console.WriteLine("Сколько снять с вашей карты?");
+                int balanceDelta = 0;
+                while(!int.TryParse(Console.ReadLine(), out balanceDelta))
+                {
+                    Console.WriteLine("Вы ввели не число!");                
+                }
+
+                balance += balanceDelta;
+                Console.WriteLine("Баланс успешно пополнен");
+            break;
+
+            default:
+            break;
+        }
     }
     else if(command == "GetChange")
     {
-        bol = false;
+        balance = 0;
     }
-    else if (command.StartsWith("BuyGood"))
+    else if(command.StartsWith("BuyGood"))
     {
         //Разбиение строки на единицы данных
-        string[] rawData = command.Substring("BuyGood".Length).Split(' ');
-
+        string[] rawData = command.Substring("BuyGood ".Length).Split(' ');
         //Сопоставление этих данных с переменными (и их типами)
         if(rawData.Length != 2)
         {
             Console.WriteLine("Неправильные аргументы команды");
             break;
         }
-
         int id = Convert.ToInt32(rawData[0]);
         int count = Convert.ToInt32(rawData[1]);
-
-        //Проверка корректности этих данных
-        //на основе текущего состояния модели.
+        //Проверка корректности этих данных на основе текущего состояния модели.
         if(id < 0 || id >= names.Length)
         {
             Console.WriteLine("Такого товара нет");
             break;
         }
-
         if(count < 0 || count > availableQuantity[id])
         {
             Console.WriteLine("Нет такого количества");
             break;
         }
-
         //Выполнение
         if(balance >= prices[id] * count)
         {
             balance -= prices[id] * count;
-            availableQuantity[id] -= count; 
+            availableQuantity[id] -= count;
         }
-
-        bol = false;
     }
     else
     {
-        Console.WriteLine("Команда неопределена");
+        Console.WriteLine("Команда не определена");
     }
 
-    //Console.ReadLine();
+    Console.ReadKey();
 }
-
-if(command == "AddMoney")
-{
-    switch (payment)
-    {
-        case PaymentType.Coins:
-        for(int i = 0; i < coinsValues.Length; i++)
-        {
-            int count = 0;
-            Console.WriteLine($"Сколько монет номиналом {coinsValues[i]} вы хотите внести?");            
-
-            while(!int.TryParse(Console.ReadLine(), out count))
-            {
-                Console.WriteLine("Вы ввели не число!");
-            }
-            
-            coinsQuantity[i] += count;
-            balance += count * coinsValues[i];
-        }
-        break;
-
-        case PaymentType.Card:
-        int balanceDelta = 0;
-        Console.WriteLine("Сколько снять свашей карты?");        
-        
-        while(!int.TryParse(Console.ReadLine(),out balanceDelta))
-        {
-            Console.WriteLine("Вы ввелине число!");
-        }
-
-        balance += balanceDelta;
-        Console.WriteLine("Баланс успешно пополнен");
-        break;
-
-        default:
-        break;  
-    }
-}
-
 
 enum PaymentType
 {
